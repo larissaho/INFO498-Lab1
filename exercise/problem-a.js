27926d42f2f79bd8625629d80c8336b4cf472313
Larissa Ho <holarissa12@gmail.com>
2019-04-03 12:04:13 -0700

    finished function

--- exercise/problem-a.js
+++ exercise/problem-a.js
@@ -1,6 +1,6 @@
 // Given an array of ints a and an int k, return if there exists a pair (x, y) in a such that x + y = k.
 // This can be solved in O(nlog(n)) time trivially, or even in O(n) if you use another data structure
-function hasPairSumToK(a: number[], k: number): boolean {
+function hasPairSumToK(a, k) {
     var i;
     var b;
     for (i = 0; i < a.length; i++) {
@@ -9,30 +9,22 @@ function hasPairSumToK(a: number[], k: number): boolean {
                 return true;
             }
         }
-    } 
+    }
     return false;
 }
-
-
-
-
-
 // TESTS don't touch them
-function test(actual: [number[],number], expected:boolean){
-    if (hasPairSumToK(actual[0], actual[1]) != expected){
-        console.log('ERROR: hasPairSumToK(',actual[0] ,',', actual[1],') should be', expected)
-    } else {
-        console.log('All tests passed!')
+function test(actual, expected) {
+    if (hasPairSumToK(actual[0], actual[1]) != expected) {
+        console.log('ERROR: hasPairSumToK(', actual[0], ',', actual[1], ') should be', expected);
+    }
+    else {
+        console.log('All tests passed!');
     }
 }
-test([[], 2], false)
-
-test([[-1, -2, -3], -5],true);
+test([[], 2], false);
+test([[-1, -2, -3], -5], true);
 test([[1, 2, 3, 4, 5, 7], 8], true);
 test([[1, 2, 3, 4, 96, -5], -4], true);
-
-
 test([[-1, -2, -3], 2], false);
 test([[1, 2, 3], 9], false);
-test([[7, 8, 9], 6],  false);
-
+test([[7, 8, 9], 6], false);
